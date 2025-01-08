dd7ed8e7451e1164dc9ca95b6037eea340f40c3e
blandinebalde <blandinebalde@gmail.com>
2025-01-08 10:34:06 +0000

    FIXING

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CarService } from '../../services/car.service';
+import { TranslateService } from '@ngx-translate/core';
+import { SharedModule } from '../../shared/shared.module';
 
 interface Car {
   id: number;
@@ -15,14 +17,26 @@ interface Car {
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [NgFor, NgIf, FormsModule, RouterModule],
-  templateUrl: './cars.component.html'
+  imports: [NgFor, NgIf, FormsModule, RouterModule, SharedModule],
+  templateUrl: './cars-v1.component.html',
+  styleUrls: ['./cars-v1.component.scss']
 })
 export class CarsComponent {
   cars: Car[] = [];
+  activeCard: number | null = null;
 
-  constructor(private carService: CarService) {
+  
+
+  constructor(private carService: CarService, private translate: TranslateService, private router: Router) {
     this.carService.getCars().subscribe(cars => this.cars = cars);
+  }
+
+  navigateToReservation() {
+    this.router.navigate(['/contact']);
+  }
+
+  toggleDiscount(index: number) {
+    this.activeCard = this.activeCard === index ? null : index;
   }
 
   selectedCategory: string = '';
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -20,6 +20,29 @@ export class NavbarComponent {
   switchLang(event: any) {
     const lang = event.target.value;
     this.translate.use(lang);
+  }
+
+  // Close menu when clicking on a nav link
+  closeMenu() {
+    this.isMenuCollapsed = true;
+  }
+
+  // Close menu when clicking outside
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const navbar = document.getElementById('navbarNav');
+    const toggleButton = document.getElementById('navbarToggler');
+
+    // If navbar and toggle button exist and are not the clicked elements
+    if (navbar && toggleButton) {
+      const clickedInsideNavbar = navbar.contains(event.target as Node);
+      const clickedToggleButton = toggleButton.contains(event.target as Node);
+
+      // Close menu if click is outside navbar and not on toggle button
+      if (!clickedInsideNavbar && !clickedToggleButton) {
+        this.isMenuCollapsed = true;
+      }
+    }
   }
 }
 
